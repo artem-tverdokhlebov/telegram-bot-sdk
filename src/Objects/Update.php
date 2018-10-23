433f222cf7a72bbc30117227a83f78d518bcdc6f
Artem Tverdokhlebov <artem@ridex.tech>
2018-10-23 20:52:42 +0300

    dev

--- src/Objects/Update.php
+++ src/Objects/Update.php
@@ -9,6 +9,7 @@ namespace Telegram\Bot\Objects;
  * @method int      getUpdateId()               The update's unique identifier. Update identifiers start from a certain positive number and increase sequentially.
  * @method Message  getMessage()                (Optional). New incoming message of any kind - text, photo, sticker, etc.
  * @method CallbackQuery getCallbackQuery()     (Optional). Callback query from a callback button in an inline keyboard
+ * @method InlineQuery getInlineQuery()         (Optional). Inline query
  */
 class Update extends BaseObject
 {
